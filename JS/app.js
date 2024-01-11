4cf8d86757c81ecabc61a3d4c07c811c9d043713
AmandaAzania <mandygudlwa@gmail.com>
2024-01-11 11:40:49 +0200

    contact form

--- JS/app.js
+++ JS/app.js
@@ -6,3 +6,11 @@ hamburger_menu.addEventListener("click", () => {
 });
 
 
+// contact form
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
